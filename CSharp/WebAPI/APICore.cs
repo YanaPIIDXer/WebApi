764e36854b2866d726d7f9e05fa7b363e3a088a7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-04-11 08:59:00 +0900

    ＡＰＩのコアクラスを追加。 ※中身はまだコンストラクタでＵＲＬのホスト部分を指定するだけ。

---
+++ CSharp/WebAPI/APICore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+	/// <summary>
+	/// ＡＰＩのコアクラス
+	/// </summary>
+	public class APICore
+	{
+
+		/// <summary>
+		/// ホスト
+		/// </summary>
+		private string Host;
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="InHost">ホスト</param>
+		APICore(string InHost)
+		{
+			Host = InHost;
+		}
+		
+	}
+}
