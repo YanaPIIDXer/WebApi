c6913bf8003d53c7ba01e0905f063df06dd19b6c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-04-12 03:08:34 +0900

    Java版WebAPIライブラリのソースとそのテスト。

---
+++ Java/Test/src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * @author YanaP
+ */
+import WebAPI.*;
+
+/**
+ * テストクラス
+ */
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		APICore core = new APICore();
+		System.out.println(core.test());
+	}
+
+}
---
+++ Java/WebAPI/src/WebAPI/APICore.java
@@ -0,0 +1,28 @@
+/**
+ * @author YanaP
+ */
+package WebAPI;
+
+/**
+ * APIコアクラス
+ */
+public class APICore
+{
+
+	/**
+	 * コンストラクタ
+	 */
+	public APICore()
+	{
+	}
+
+	/**
+	 * テスト用メソッド
+	 * @return テスト用文字列
+	 */
+	public String test()
+	{
+		return "WebAPI Test";
+	}
+
+}
